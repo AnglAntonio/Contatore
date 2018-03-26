497a08ff13d1768768efcf372ca43d65c67e5075
Angelo <marinotonio@gmail.com>
2018-03-26 15:27:40 +0200

    Ho impostato i riferimenti

--- app/src/main/java/com/example/angelo/contatore/Contatore.java
+++ app/src/main/java/com/example/angelo/contatore/Contatore.java
@@ -2,12 +2,40 @@ package com.example.angelo.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Contatore extends AppCompatActivity {
+
+
+    //View Layout
+    private TextView Messaggio;
+    private Button Aumenta;
+    private Button Diminuisci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contatore);
+
+        //imposto riferimenti layout
+        Messaggio = findViewById(R.id.textMessaggio);
+        Aumenta = findViewById(R.id.buttonAumenta);
+        Diminuisci = findViewById(R.id.buttonDiminuisci);
+
+
+
+
+
+
     }
+
+
+
+
+
+
+
+
 }
+
